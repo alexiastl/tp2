29cd0599936c806d9fa297f8be3e17930f6d6d6a
alexisstl <>
2020-06-20 20:57:48 -0400

    insert et FK

--- tp2_test.sql
+++ tp2_test.sql
@@ -71,16 +71,8 @@ create table TP2_OEUVRE (
     SYNOPSYS_OEU varchar2(500) not null,
     DUREE_OEU number(3,0) not null,
     CLASSEMENT_OEU number(2,1) null,
-    COMPAGNIE_OEU number(2,1) not null,
+    COMPAGNIE_OEU varchar2(30) not null,
     constraint PK_OEUVRE primary key (NO_OEUVRE)
-);
-
-create table TP2_REALISATEUR_OEUVRE (
-    NO_OEUVRE number(6,0) not null,
-    NO_REALISATEUR number(6,0) not null, 
-    constraint PK_RO_OEU_REA primary key( NO_OEUVRE, NO_REALISATEUR)/*,
-    constraint FK_RO_NO_OEUVRE foreign key(NO_OEUVRE) references TP2_OEUVRE (NO_OEUVRE),
-    constraint FK_RO_NO_REA foreign key (NO_REALISATEUR) references REALISATEUR (NO_REALISATEUR)*/
 );
 
 create table TP2_REALISATEUR (
@@ -90,14 +82,10 @@ create table TP2_REALISATEUR (
     constraint PK_REALISATEUR primary key (NO_REALISATEUR)
 );
 
-create table TP2_ROLE_OEUVRE (
+create table TP2_REALISATEUR_OEUVRE (
     NO_OEUVRE number(6,0) not null,
-    PERSONNAGE varchar2(50) not null,
-    NO_ACTEUR number(6,0) not null,
-    constraint PK_ROLE_OEUVRE primary key (NO_OEUVRE, PERSONNAGE)/*,
-    constraint FK_RO_NO_OEUVRE foreign key (NO_OEUVRE) references OEUVRE (NO_OEUVRE),
-    constraint FK_RO_NO_ACTEUR foreign key (NO_ACTEUR) references ACTEUR (NO_ACTEUR)
-*/
+    NO_REALISATEUR number(6,0) not null, 
+    constraint PK_RO_OEU_REA primary key( NO_OEUVRE, NO_REALISATEUR)
 );
 
 create table TP2_ACTEUR (
@@ -107,6 +95,15 @@ create table TP2_ACTEUR (
     DATE_NAISSANCE_ACT date not null,
     constraint PK_ACTEUR primary key (NO_ACTEUR) 
 );
+
+create table TP2_ROLE_OEUVRE (
+    NO_OEUVRE number(6,0) not null,
+    PERSONNAGE varchar2(50) not null,
+    NO_ACTEUR number(6,0) not null,
+    constraint PK_ROLE_OEUVRE primary key (NO_OEUVRE, PERSONNAGE)
+);
+
+
 
 create table TP2_UTILISATEUR (
     LOGIN_UTILISATEUR varchar2(10)not null, /*A VERIFIER*/
@@ -127,14 +124,11 @@ create table TP2_CRITIQUE(
     DATE_CRI date default sysdate not null,
     COTE_CRI number(2,1) not null, 
     COMMENTAIRE_CRI VARCHAR2(500) null,
-    REPOND_A_NO_CRITIQUE NUMBER(6,0) null,/*
+    REPOND_A_NO_CRITIQUE NUMBER(6,0) null,
     constraint PK_CRITIQUE primary key(NO_CRITIQUE),
-    constraint FK_CRI_NO_OEUVRE foreign key (NO_OEUVRE) references OEUVRE(NO_OEUVRE),
-    constraint FK_CRI_SURNOM_MEM foreign key (SURNOM_MEMBRE) references MEMBRE (SURNOM_MEMBRE),
-    constraint FK_CRI_REPOND_A foreign key (REPOND_A_NO_CRITIQUE) references CRITIQUE(NO_CRITIQUE),*/
     constraint AK_CRITIQUE unique (NO_OEUVRE, SURNOM_MEMBRE, DATE_CRI)
 );
-/*SÉPARATION*/
+
 alter table TP2_HORAIRE_PLATEFORME
     add (constraint FK_PLA_NOM_PLATEFORME foreign key (NOM_PLATEFORME) references TP2_PLATEFORME(NOM_PLATEFORME),
     constraint FK_PLA_NO_OEUVRE foreign key (NO_OEUVRE) references TP2_OEUVRE(NO_OEUVRE));
@@ -149,7 +143,28 @@ alter table TP2_HORAIRE_CINEMA
     
 alter table TP2_BILLET_CINEMA
     add constraint FK_BIL_NOM_CINEMA foreign key (NOM_CINEMA) references TP2_CINEMA(NOM_CINEMA);
+
+alter table TP2_REALISATEUR_OEUVRE
+    add constraint FK_REA_O_NO_OEUVRE foreign key(NO_OEUVRE) references TP2_OEUVRE (NO_OEUVRE);
     
+alter table TP2_REALISATEUR_OEUVRE
+    add constraint FK_REA_O_NO_REA foreign key (NO_REALISATEUR) references TP2_REALISATEUR (NO_REALISATEUR);
+    
+alter table TP2_ROLE_OEUVRE
+    add constraint FK_ROL_NO_OEUVRE foreign key (NO_OEUVRE) references TP2_OEUVRE (NO_OEUVRE);
+    
+alter table TP2_ROLE_OEUVRE
+    add constraint FK_RO_NO_ACTEUR foreign key (NO_ACTEUR) references TP2_ACTEUR (NO_ACTEUR);
+
+alter table TP2_CRITIQUE 
+    add constraint FK_CRI_NO_OEUVRE foreign key (NO_OEUVRE) references TP2_OEUVRE(NO_OEUVRE);
+    
+
+
+alter table TP2_CRITIQUE 
+    add constraint FK_CRI_REPOND_A foreign key (REPOND_A_NO_CRITIQUE) references TP2_CRITIQUE(NO_CRITIQUE);
+
+
 create or replace view TP2_VUE_MEMBRE (SURNOM_MEMBRE, NOM_MEM, PRENOM_MEM, COURRIEL_MEM, DATE_NAISSANCE_MEM, MOT_DE_PASSE_MEM) as
     select LOGIN_UTILISATEUR, NOM_UTI, PRENOM_UTI, COURRIEL_UTI, DATE_NAISSANCE_UTI, MOT_DE_PASSE_UTI
         from TP2_UTILISATEUR
@@ -159,7 +174,38 @@ create or replace view TP2_VUE_EMPLOYE (LOGIN_EMPLOYE, PRENOM_EMP, NOM_EMP, COUR
     select LOGIN_UTILISATEUR, PRENOM_UTI, NOM_UTI, COURRIEL_UTI, MOT_DE_PASSE_UTI
         from TP2_UTILISATEUR
             where TYPE_UTI = 'Employé';
+
+alter table TP2_CRITIQUE 
+    add constraint FK_CRI_SURNOM_MEM foreign key (SURNOM_MEMBRE) references TP2_VUE_MEMBRE(SURNOM_MEMBRE);
     
 create sequence NO_CRITIQUE_SEQ
     start with 5 
     increment by 5 ;
+
+insert into TP2_OEUVRE( TITRE_OEU, ANNEE_OEU, GENRE_OEU, SYNOPSYS_OEU, DUREE_OEU, CLASSEMENT_OEU, COMPAGNIE_OEU) 
+    values ('Titanic',1997,'Romance',
+        'Southampton, 10 avril 1912. Le paquebot le plus grand et le plus moderne du monde, réputé pour son insubmersibilité, 
+        le "Titanic", appareille pour son premier voyage. Quatre jours plus tard, il heurte un iceberg. 
+        A son bord, un artiste pauvre et une grande bourgeoise tombent amoureux.',188,7.8,'Paramount Pictures');
+
+insert into TP2_OEUVRE( TITRE_OEU, ANNEE_OEU, GENRE_OEU, SYNOPSYS_OEU, DUREE_OEU, CLASSEMENT_OEU, COMPAGNIE_OEU) 
+    values ('Joker',2019,'drame',
+        'Le film, qui relate une histoire originale inédite sur grand écran, se focalise sur la figure emblématique de 
+        l''ennemi juré de Batman. Il brosse le portrait d''Arthur Fleck, un homme sans concession méprisé par la société. ',
+        122,8.5,'DC FIlms');
+
+insert into TP2_REALISATEUR (NOM_REA, PRENOM_REA)
+    values ('Cameron','James');
+    
+insert into TP2_REALISATEUR (NOM_REA, PRENOM_REA)
+    values ('Phillips','Todd');
+    
+insert into TP2_REALISATEUR_OEUVRE 
+    VALUES(1,1);
+    
+insert into TP2_REALISATEUR_OEUVRE 
+    VALUES(2,2);
+
+insert into TP2_ACTEUR (NOM_ACT, PRENOM_ACT,DATE_NAISSANCE_ACT)
+    values ('DiCaprio','Leonardo',to_date('74-11-11','YY-MM-DD'));
+
